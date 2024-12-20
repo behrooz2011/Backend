0ef11c96f2b2285231e0c9d72c29bf6c068c2e9c
behrooz2011 <bmmm1370@gmail.com>
2024-12-20 11:21:17 -0500

    username and email changed successfully, green squares will be registered from now on

--- 2-currency-convertor/routes/userRoutes.js
+++ 2-currency-convertor/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { signup, login } = require("../controllers/userController");
 const router = express.Router();
 
-//direct functions
+//direct functions, no need to import the files name
 router.post("/signup", signup);
 router.post("/login", login);
 
