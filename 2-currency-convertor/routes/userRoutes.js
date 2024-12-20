9a9671c94100e8132ba08ce470b5b616d1819766
behrooz2011 <132863666+CodeMasterYoutube@users.noreply.github.com>
2024-12-20 11:15:13 -0500

    username changed

--- 2-currency-convertor/routes/userRoutes.js
+++ 2-currency-convertor/routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { signup, login } = require("../controllers/userController");
 const router = express.Router();
 
+//direct functions
 router.post("/signup", signup);
 router.post("/login", login);
 
