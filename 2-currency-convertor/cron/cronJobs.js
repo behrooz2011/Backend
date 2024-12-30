f8e27f114588a3744dab88c07fab02ae4f07a36b
behrooz2011 <bmmm1370@gmail.com>
2024-12-29 19:21:01 -0500

    Deletion added for active alert, Next, we'll replace the cron job codes with async/await

--- 2-currency-convertor/cron/cronJobs.js
+++ 2-currency-convertor/cron/cronJobs.js
@@ -1,3 +1,4 @@
+//############################### order #######################
 const cron = require("node-cron");
 const nodemailer = require("nodemailer");
 const exchangeService = require("../services/exchangeService");
@@ -11,24 +12,22 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-//Run every minute
+// Run every minute
 exports.startRateAlertChecker = () => {
   cron.schedule("* * * * *", async () => {
     console.log("Checking rate alerts...");
     try {
       const rates = await exchangeService.getExchangeRates();
-
       db.all("SELECT * FROM alerts", [], (err, alerts) => {
         if (err) throw err;
-        console.log("alerts: ", alerts);
+        console.log("alerts: ", alerts.length);
         alerts.forEach((alert) => {
-          // const [from, to] = alert.pair.split("_");
           const currentRate =
             rates.rates[alert.to_currency] / rates.rates[alert.from_currency];
           console.log("currentRate: ", currentRate);
 
           if (
-            (alert.rateType === "below" && currentRate <= alert.target_rate) || //alert.rate comes from the table, ie, user generated
+            (alert.rateType === "below" && currentRate <= alert.target_rate) ||
             (alert.rateType === "above" && currentRate >= alert.target_rate)
           ) {
             db.get(
@@ -36,6 +35,7 @@ exports.startRateAlertChecker = () => {
               [alert.user_id],
               (err, user) => {
                 if (err) throw err;
+                console.log(`Alert for ${alert.user_id}: `);
 
                 const mailOptions = {
                   from: process.env.EMAIL_USER,
@@ -46,10 +46,27 @@ exports.startRateAlertChecker = () => {
                   } is now ${currentRate.toFixed(4)}.`,
                 };
 
-                console.log("---Email Sent ----");
                 transporter.sendMail(mailOptions, (err) => {
-                  if (err) console.error(err.message);
-                  else console.log(`Notification sent to ${user.email}`);
+                  if (err) {
+                    console.log("email error--");
+                    console.error(err.message);
+                  } else {
+                    console.log(`Notification sent to ${user.email}`);
+                    // Remove the alert from the database after successful email
+                    db.run(
+                      "DELETE FROM alerts WHERE id = ?",
+                      [alert.id],
+                      (err) => {
+                        if (err) {
+                          console.error("Error deleting alert:", err.message);
+                        } else {
+                          console.log(
+                            `Alert with ID ${alert.id} removed from the database.`
+                          );
+                        }
+                      }
+                    );
+                  }
                 });
               }
             );
@@ -61,3 +78,90 @@ exports.startRateAlertChecker = () => {
     }
   });
 };
+
+// ********************* Async/ Await version******************************
+// const cron = require("node-cron");
+// const nodemailer = require("nodemailer");
+// const exchangeService = require("../services/exchangeService");
+// const db = require("../db");
+
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
+// // Helper function to wrap db calls in a promise
+// const dbAll = (query, params) => {
+//   return new Promise((resolve, reject) => {
+//     db.all(query, params, (err, rows) => {
+//       if (err) reject(err);
+//       else resolve(rows);
+//     });
+//   });
+// };
+
+// const dbGet = (query, params) => {
+//   return new Promise((resolve, reject) => {
+//     db.get(query, params, (err, row) => {
+//       if (err) reject(err);
+//       else resolve(row);
+//     });
+//   });
+// };
+
+// const dbRun = (query, params) => {
+//   return new Promise((resolve, reject) => {
+//     db.run(query, params, function (err) {
+//       if (err) reject(err);
+//       else resolve(this);
+//     });
+//   });
+// };
+
+// // Run every minute
+// exports.startRateAlertChecker = () => {
+//   cron.schedule("* * * * *", async () => {
+//     console.log("Checking rate alerts...");
+//     try {
+//       const rates = await exchangeService.getExchangeRates();
+//       const alerts = await dbAll("SELECT * FROM alerts", []);
+
+//       console.log("alerts: ", alerts);
+//       for (const alert of alerts) {
+//         const currentRate =
+//           rates.rates[alert.to_currency] / rates.rates[alert.from_currency];
+//         console.log("currentRate: ", currentRate);
+
+//         if (
+//           (alert.rateType === "below" && currentRate <= alert.target_rate) ||
+//           (alert.rateType === "above" && currentRate >= alert.target_rate)
+//         ) {
+//           const user = await dbGet("SELECT email FROM users WHERE id = ?", [alert.user_id]);
+//           console.log(`Alert for ${alert.user_id}: `, alert);
+
+//           const mailOptions = {
+//             from: process.env.EMAIL_USER,
+//             to: user.email,
+//             subject: "Rate Alert Notification",
+//             text: `The exchange rate for ${alert.pair} is now ${currentRate.toFixed(4)}.`,
+//           };
+
+//           try {
+//             await transporter.sendMail(mailOptions);
+//             console.log(`Notification sent to ${user.email}`);
+//             // Remove the alert from the database after successful email
+//             await dbRun("DELETE FROM alerts WHERE id = ?", [alert.id]);
+//             console.log(`Alert with ID ${alert.id} removed from the database.`);
+//           } catch (err) {
+//             console.error("Error sending email or deleting alert:", err.message);
+//           }
+//         }
+//       }
+//     } catch (error) {
+//       console.error("Error checking rate alerts:", error.message);
+//     }
+//   });
+// };
